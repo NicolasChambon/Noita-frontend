8f5fc1089b76dcf38b6ad671b24f700f72fc5f6b
NicolasChambon <nico.chambon@gmail.com>
2024-08-27 23:35:15 +0200

    desktop integration of the carousel

--- src/components/pages/Home/Carousel/Carousel.jsx
+++ src/components/pages/Home/Carousel/Carousel.jsx
@@ -1,19 +1,48 @@
-import ReactPlayer from 'react-player';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import './Carousel.scss';
 
+import img1 from '../../../../assets/carousel/noita_vertanzt_1.jpg';
+import img2 from '../../../../assets/carousel/noita_vertanzt_2.jpg';
+import img3 from '../../../../assets/carousel/noita_vertanzt_3.jpg';
+import img4 from '../../../../assets/carousel/noita_vertanzt_4.jpg';
+import img5 from '../../../../assets/carousel/noita_vertanzt_5.jpg';
+import img6 from '../../../../assets/carousel/noita_vertanzt_6.jpg';
+
 const Carousel = () => {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 5000,
+    className: 'Carousel-slider',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '50px',
+    slidesToShow: 1,
+    speed: 1000,
+  };
+
   return (
     <div className="Carousel">
-      <ReactPlayer
-        className="Carousel-player"
-        url="https://www.youtube.com/embed/djUySOtNbj8"
-        width="100%"
-        height="100%"
-        controls={true}
-        light={false}
-        playing={false}
-      />
+      <Slider {...settings}>
+        <div>
+          <img src={img1} alt="Noita Vertanzt 1" />
+        </div>
+        <div>
+          <img src={img2} alt="Noita Vertanzt 2" />
+        </div>
+        <div>
+          <img src={img3} alt="Noita Vertanzt 3" />
+        </div>
+        <div>
+          <img src={img4} alt="Noita Vertanzt 4" />
+        </div>
+        <div>
+          <img src={img5} alt="Noita Vertanzt 5" />
+        </div>
+      </Slider>
     </div>
   );
 };
