d7cb5f773d41e8243cf4b0ca21bc917274b58cb7
NicolasChambon <nico.chambon@gmail.com>
2024-08-27 13:38:08 +0200

    identity is responsive / setup header navlink (logo and identity)

--- src/components/organisms/Header/HeaderLogo/HeaderLogo.jsx
+++ src/components/organisms/Header/HeaderLogo/HeaderLogo.jsx
@@ -5,8 +5,19 @@ import NoitaLogo from '../../../../assets/logos/noita-logo.svg';
 import './HeaderLogo.scss';
 
 const HeaderLogo = () => {
+  const handleLogoClick = () => {
+    if (window.location.pathname === '/') {
+      const page = document.querySelector('.App');
+      page.scrollIntoView({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <Link className="HeaderLogo">
+    <Link className="HeaderLogo" onClick={handleLogoClick}>
       <img className="HeaderLogo-img" src={NoitaLogo} alt="Noita Logo" />
     </Link>
   );
--- src/components/organisms/Header/HeaderMenu/MenuNav/MenuNav.jsx
+++ src/components/organisms/Header/HeaderMenu/MenuNav/MenuNav.jsx
@@ -3,21 +3,38 @@ import { Link, NavLink } from 'react-router-dom';
 import './MenuNav.scss';
 
 const MenuNav = () => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <nav className="MenuNav">
       <ul className="MenuNav-list">
         <li className="MenuNav-list-item">
-          <Link className="MenuNav-list-item-link" to="#identity">
+          <Link
+            className="MenuNav-list-item-link"
+            to="#"
+            onClick={() => scrollToSection('identity')}
+          >
             Identité
           </Link>
         </li>
         <li className="MenuNav-list-item">
-          <Link className="MenuNav-list-item-link" to="#dates">
+          <Link
+            className="MenuNav-list-item-link"
+            to="#"
+            onClick={() => scrollToSection('dates')}
+          >
             Concerts
           </Link>
         </li>
         <li className="MenuNav-list-item">
-          <Link className="MenuNav-list-item-link" to="#contact">
+          <Link
+            className="MenuNav-list-item-link"
+            to="#"
+            onClick={() => scrollToSection('contact')}
+          >
             Contact
           </Link>
         </li>
