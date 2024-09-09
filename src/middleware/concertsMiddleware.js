c5f1808758112c31d5a3dc2072a96548960fad4c
NicolasChambon <nico.chambon@gmail.com>
2024-09-09 14:36:14 +0200

    handle session expiration

--- src/middleware/concertsMiddleware.js
+++ src/middleware/concertsMiddleware.js
@@ -1,4 +1,5 @@
 import { FETCH_CONCERTS, storeConcerts } from '../actions/concertsActions';
+import { logout, loginFailure } from '../actions/loginActions';
 
 const concertsMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
@@ -16,6 +17,14 @@ const concertsMiddleware = (store) => (next) => (action) => {
 
           if (!response.ok) {
             const error = await response.json();
+            if (error.status === 401) {
+              localStorage.removeItem('token');
+              store.dispatch(logout());
+              store.dispatch(storeConcerts([]));
+              store.dispatch(
+                loginFailure(['The session has expired, please log in again.']),
+              );
+            }
             throw new Error(error.errors);
           }
 
