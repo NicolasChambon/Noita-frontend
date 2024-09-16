7563a3a3391c2730281e610eb44d1a25fd3505c8
NicolasChambon <nico.chambon@gmail.com>
2024-09-16 11:30:39 +0200

    before merge on main and deploy

--- src/components/pages/Home/Media/Carousel/Carousel.jsx
+++ src/components/pages/Home/Media/Carousel/Carousel.jsx
@@ -34,14 +34,11 @@ const Carousel = () => {
     speed: 1000,
   };
 
-  console.log(pictures);
-
   return (
     <div className="Carousel">
       <Slider {...settings}>
         {pictures &&
           pictures.map((picture) => {
-            console.log(`${import.meta.env.VITE_URL}${picture.url}`);
             return (
               <div className="Carousel-slider-card" key={picture.id}>
                 <img
@@ -51,41 +48,6 @@ const Carousel = () => {
               </div>
             );
           })}
-        {/* <div className="Carousel-slider-card">
-          <img
-            className="Carousel-slider-card-img"
-            src={img2}
-            alt="Noita Vertanzt 2"
-          />
-        </div>
-        <div className="Carousel-slider-card">
-          <img
-            className="Carousel-slider-card-img"
-            src={img3}
-            alt="Noita Vertanzt 3"
-          />
-        </div>
-        <div className="Carousel-slider-card">
-          <img
-            className="Carousel-slider-card-img"
-            src={img4}
-            alt="Noita Vertanzt 4"
-          />
-        </div>
-        <div className="Carousel-slider-card">
-          <img
-            className="Carousel-slider-card-img"
-            src={img5}
-            alt="Noita Vertanzt 5"
-          />
-        </div>
-        <div className="Carousel-slider-card">
-          <img
-            className="Carousel-slider-card-img"
-            src={img6}
-            alt="Noita Vertanzt 6"
-          />
-        </div> */}
       </Slider>
     </div>
   );
