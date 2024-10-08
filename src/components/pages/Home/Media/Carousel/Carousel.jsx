8594bc2fba1aa6f49fccfffe515c5c145d56129a
NicolasChambon <nico.chambon@gmail.com>
2024-10-08 11:54:17 +0200

    new Carousel version done

--- src/components/pages/Home/Media/Carousel/Carousel.jsx
+++ src/components/pages/Home/Media/Carousel/Carousel.jsx
@@ -1,5 +1,5 @@
 // Dependencies
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ImageGallery from 'react-image-gallery';
 
@@ -13,9 +13,13 @@ import './Carousel.scss';
 const Carousel = () => {
   // Hooks
   const dispatch = useDispatch();
+  const galleryRef = useRef(null);
 
   // Redux state
   const pictures = useSelector((state) => state.carousel.pictureList);
+
+  // Local state
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   // Fetch carousel pictures
   useEffect(() => {
@@ -28,10 +32,36 @@ const Carousel = () => {
     };
   });
 
+  // Toggle fullscreen mode and autoplay when clicking on the gallery
+  const handleGalleryClick = () => {
+    if (galleryRef.current) {
+      galleryRef.current.toggleFullScreen();
+      setIsFullscreen(!isFullscreen);
+      const newIsFullscreen = !isFullscreen;
+
+      newIsFullscreen ? galleryRef.current.pause() : galleryRef.current.play();
+    }
+  };
+
   return (
-    <div className="Carousel">
-      <ImageGallery items={images} />
-    </div>
+    <ImageGallery
+      additionalClass="Carousel"
+      autoPlay={true}
+      disableSwipe={false}
+      flickThreshold={0.4}
+      items={images}
+      lazyLoad={true}
+      onClick={handleGalleryClick}
+      ref={galleryRef}
+      showFullscreenButton={false}
+      showIndex={false}
+      showPlayButton={false}
+      showThumbnails={false}
+      slideDuration={500}
+      slideInterval={5000}
+      swipeThreshold={30}
+      useBrowserFullscreen={true}
+    />
   );
 };
 
